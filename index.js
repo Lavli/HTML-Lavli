563bf38e9a6c489b4a8b09b41c9310a2df79e727
Lavli <57790799+Lavli@users.noreply.github.com>
2019-11-20 10:34:30 +0200

    Update

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ $("button").click(function(){
+     $(".box")
+         .animate({width: "300px"})
+         .animate({height: "300px"})
+         .animate({marginLeft: "150px"})
+         .animate({borderWidth: "10px"})
+         .animate({opacity: 0.5});
+ });
+});
